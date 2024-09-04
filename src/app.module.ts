dbe83191acd46c10bb5a30b79f2021bf39c1d6db
Israel Batista <israelbatiista19@gmail.com>
2024-09-04 15:16:19 -0300

    feat: add file specific module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AuthCredentialCommand } from './commands/auth/login/command/auth-credential.command';
-import { PrismaService } from './lib/prisma.service';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { GoogleAuthCommand } from './commands/auth/login/command/auth-google.command';
-import { GoogleAuthCallback } from './commands/auth/login/services/GoogleAuthCallback';
-
-import { OpenAIService } from './lib/openai.service';
+import { ConfigModule } from '@nestjs/config';
 
 import { BranchModule } from './branches/branch.module';
 import { RepoModule } from './repos/repo.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => ({
-        secret: config.get('JWT_SECRET'),
-        signOptions: { expiresIn: '7d' },
-      }),
-      inject: [ConfigService],
-    }),
+
     BranchModule,
     RepoModule,
+    AuthModule,
   ],
   controllers: [],
-  providers: [
-    AuthCredentialCommand,
-    GoogleAuthCommand,
-    PrismaService,
-    OpenAIService,
-    GoogleAuthCallback,
-  ],
+  providers: [],
 })
 export class AppModule {}
